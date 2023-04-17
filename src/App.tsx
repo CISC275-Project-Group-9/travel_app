683255292724f4839ad4f72f5309a636886911cf
sneha <snehnang@udel.edu>
2023-04-17 11:46:32 -0400

    Added name to site

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,8 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                Travel App Cisc275
-            </header>
+            <header className="App-header">Travel App Cisc275</header>
+            <p>Team: Benita Abraham, Sneha Nangelimalil</p>
             <hr></hr>
         </div>
     );
