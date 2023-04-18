094f15c09dc9efd61d1220b75c91186267430f34
sneha <snehnang@udel.edu>
2023-04-18 14:37:21 -0400

    Super List basic component

---
+++ src/components/SuperList.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { useState } from "react";
+import { Destination } from "../interfaces/destination";
+
+export function SuperList(): JSX.Element {
+    const [centralList, setCentralList] = useState<Destination[]>([]);
+    const [itinerary, setItinerary] = useState<Destination[]>([]);
+    const [userList, setUsers] = useState<Destination[]>([]);
+
+    function seeUsers() {
+        setUsers(userList);
+    }
+
+    function newDestinationToList(newDestination: Destination) {
+        if (!centralList.includes(newDestination)) {
+            const newCentralList = [...centralList, newDestination];
+            setCentralList(newCentralList);
+        }
+    }
+
+    function reviewDestination(Destination: Destination) {
+        if (centralList.includes(Destination)) {
+            const newCentralList = [...centralList, Destination];
+            setCentralList(newCentralList);
+        }
+    }
+
+    function addDestination(newDestination: Destination) {
+        if (!itinerary.includes(newDestination)) {
+            const newItinerary = [...itinerary, newDestination];
+            setItinerary(newItinerary);
+        }
+    }
+
+    function removeDestination(destination: Destination) {
+        if (itinerary.includes(destination)) {
+            const id = destination.id;
+            const newItinerary = itinerary.filter(
+                (dest: Destination): boolean => dest.id !== id
+            );
+            setItinerary(newItinerary);
+        }
+    }
+
+    function clearItinerary() {
+        setItinerary([]);
+    }
+
+    return (
+        <div>
+            <h3>Destinations:</h3>
+            <ul>
+                {centralList.map((destination: Destination) => (
+                    <li key={destination.id}>{destination.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
