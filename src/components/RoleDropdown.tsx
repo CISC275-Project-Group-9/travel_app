deabf6e6346e3ddd122daf3a06a3dd124f1f59f8
chrismun <chrismun@udel.edu>
2023-04-25 00:46:45 -0400

    Added drag and drop to lists and did some css

--- src/components/RoleDropdown.tsx
+++ src/components/RoleDropdown.tsx
@@ -1,31 +1,36 @@
 import React, { useState } from "react";
-import { UserList } from "./UserList";
+import UserList from "./UserList";
 import { Form } from "react-bootstrap";
 import { AdminList } from "./AdminList";
 import { SuperList } from "./SuperList";
+import "./RoleDropdown.css"; 
 
 export function RoleDropdown(): JSX.Element {
     const ROLES = ["Basic", "Staff", "Faculty"];
     const DEFAULT_ROLE = ROLES[0];
     const [roleType, setRoleType] = useState<string>(DEFAULT_ROLE);
+  
     function changeRole(event: React.ChangeEvent<HTMLSelectElement>) {
-        setRoleType(event.target.value);
+      setRoleType(event.target.value);
     }
+  
     return (
-        <div>
-            <Form.Group controlId="roleSelect">
-                <Form.Label>Choose your role</Form.Label>
-                <Form.Select value={roleType} onChange={changeRole}>
-                    {ROLES.map((role: string) => (
-                        <option key={role} value={role}>
-                            {role}
-                        </option>
-                    ))}
-                </Form.Select>
-            </Form.Group>
-            {roleType === "Basic" ? <UserList></UserList> : null}
-            {roleType === "Staff" ? <AdminList></AdminList> : null}
-            {roleType === "Faculty" ? <SuperList></SuperList> : null}
+      <div className="role-dropdown">
+        <div className="dropdown-label">Choose your role:</div>
+        <Form.Group controlId="roleSelect">
+          <Form.Select value={roleType} onChange={changeRole}>
+            {ROLES.map((role: string) => (
+              <option key={role} value={role}>
+                {role}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
+        <div className="list-container">
+          {roleType === "Basic" ? <UserList></UserList> : null}
+          {roleType === "Staff" ? <AdminList></AdminList> : null}
+          {roleType === "Faculty" ? <SuperList></SuperList> : null}
         </div>
+      </div>
     );
-}
+  }
