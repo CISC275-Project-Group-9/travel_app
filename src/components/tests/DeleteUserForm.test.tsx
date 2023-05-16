7c08e61fca9efa68e505e35c8424ff7685fc3d60
benita <beniabra@udel.edu>
2023-05-16 09:17:13 -0400

    finished testing DeleteUserForm

--- src/components/tests/DeleteUserForm.test.tsx
+++ src/components/tests/DeleteUserForm.test.tsx
@@ -1,12 +1,86 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { ReactElement, ReactNode } from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { DndProvider } from "react-dnd";
 import userEvent from "@testing-library/user-event";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import { DeleteUserForm } from "../DeleteUserForm";
+import { RoleDropdown, User } from "../RoleDropdown";
 
-// describe("DeleteUserForm Component tests", () => {
-    
-    
-// })
+const users = [
+    {
+      id: 1,
+      name: "Default Basic User",
+      role: "Basic",
+      itinerary: [],
+    },
+    {
+      id: 2,
+      name: "Default Staff User",
+      role: "Staff",
+      itinerary: [],
+    },
+    {
+      id: 3,
+      name: "Default Faculty User",
+      role: "Faculty",
+      itinerary: [],
+    },
+  ]
+
+describe("DeleteUserForm Component tests", () => {
+    test("There is a delete user label", () => {
+        render(
+            <DndProvider backend={HTML5Backend}>
+                <DeleteUserForm onSubmit={function (u: User): void {
+                    throw new Error("Function not implemented.");
+                } } users={users} />
+            </DndProvider>
+        );
+        expect(screen.getByText(/Delete User:/i)).toBeInTheDocument();
+    });
+    test("There is a user dropdown", () => {
+        render(
+            <DndProvider backend={HTML5Backend}>
+                <DeleteUserForm onSubmit={function (u: User): void {
+                    throw new Error("Function not implemented.");
+                } } users={users} />
+            </DndProvider>
+        );
+        expect(screen.getByTestId("deleteUserDropdown")).toBeInTheDocument();
+    });
+    test("There are 3 roles to delete", () => {
+        render(
+            <DndProvider backend={HTML5Backend}>
+                <DeleteUserForm onSubmit={function (u: User): void {
+                    throw new Error("Function not implemented.");
+                } } users={users} />
+            </DndProvider>
+        );
+        const dropdown = screen.getByTestId("deleteUserDropdown") as HTMLSelectElement  
+        expect(dropdown.options.length).toEqual(3); 
+    });
+    test("There is delete button", () => {
+        render(
+            <DndProvider backend={HTML5Backend}>
+                <DeleteUserForm onSubmit={function (u: User): void {
+                    throw new Error("Function not implemented.");
+                } } users={users} />
+            </DndProvider>
+        );
+        expect(screen.getByRole("button")).toBeInTheDocument();
+    });
+    test("Can delete user", () => {
+        render(
+            <DndProvider backend={HTML5Backend}>
+                <DeleteUserForm onSubmit={function (u: User): void {
+                    throw new Error("Function not implemented.");
+                } } users={users} />
+            </DndProvider>
+        ); 
+        const dropdown = screen.getByTestId("deleteUserDropdown") as HTMLSelectElement
+        fireEvent.change(dropdown, {target: {value: "Default Basic User"}})
+        const button = screen.getByRole("button", {name: "Delete User"});
+        expect(dropdown.options.length).toEqual(3);
+    });
+})
